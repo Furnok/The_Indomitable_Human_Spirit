38dd111907363fdf3436b0233135fe297efe17f0
Killian FURNO <killian.furno@orange.fr>
2026-01-19 11:40:50 +0100

    Fix: Camera

--- Assets/App/Scripts/Runtime/Containers/Camera/S_ClassCameraData.cs
+++ Assets/App/Scripts/Runtime/Containers/Camera/S_ClassCameraData.cs
@@ -1,6 +1,5 @@
 ﻿using Sirenix.OdinInspector;
 using System;
-using UnityEngine;
 
 [Serializable]
 public class S_ClassCameraData
@@ -8,11 +7,19 @@ public class S_ClassCameraData
     [Title("Camera")]
     public float cameraDistanceMinPlayer = 0;
 
+    [Title("Camera Targeting")]
+    public float shoulderOffsetAmountPositive = 0;
+
+    public float shoulderOffsetAmountNegative = 0;
+
     [SuffixLabel("°", Overlay = true)]
     public float minVerticalCameraPlayer = 0;
 
     [SuffixLabel("°", Overlay = true)]
     public float maxVerticalCameraPlayer = 0;
+
+    [SuffixLabel("s", Overlay = true)]
+    public float offsetTransitionTime = 0f;
 
     [Title("Cinematic")]
     [SuffixLabel("s", Overlay = true)]
