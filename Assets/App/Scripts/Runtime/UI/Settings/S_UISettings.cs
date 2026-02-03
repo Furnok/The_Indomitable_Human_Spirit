7571d14bebe03863c599a2ee1bc7cbb65e327926
Killian FURNO <killian.furno@orange.fr>
2026-02-03 09:45:04 +0100

    Fix: Settings

--- Assets/App/Scripts/Runtime/UI/Settings/S_UISettings.cs
+++ Assets/App/Scripts/Runtime/UI/Settings/S_UISettings.cs
@@ -202,7 +202,7 @@ public class S_UISettings : MonoBehaviour
     {
         if (rsoInConsole.Value) return;
 
-        if (rsoInConsole.Value && dropDownLanguages?.GetComponent<TMP_Dropdown>()?.IsExpanded == true || dropDownResolutions?.GetComponent<TMP_Dropdown>()?.IsExpanded == true || dropDownQuality?.GetComponent<TMP_Dropdown>()?.IsExpanded == true)
+        if (dropDownLanguages?.GetComponent<TMP_Dropdown>()?.IsExpanded == true || dropDownResolutions?.GetComponent<TMP_Dropdown>()?.IsExpanded == true || dropDownQuality?.GetComponent<TMP_Dropdown>()?.IsExpanded == true)
         {
             return;
         }
