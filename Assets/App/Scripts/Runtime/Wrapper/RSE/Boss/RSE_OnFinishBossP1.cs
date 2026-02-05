ad02c93476a5f33b550d320e16661dcbf360eedf
Killian FURNO <killian.furno@orange.fr>
2026-02-05 22:05:24 +0100

    Fix: Add Events Prep Boss

---
+++ Assets/App/Scripts/Runtime/Wrapper/RSE/Boss/RSE_OnFinishBossP1.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_OnFinishBossP1", menuName = "Data/RSE/Boss/OnFinishBossP1")]
+public class RSE_OnFinishBossP1 : BT.ScriptablesObject.RuntimeScriptableEvent{}
---
+++ Assets/App/Scripts/Runtime/Wrapper/RSE/Boss/RSE_OnFinishBossP2.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_OnFinishBossP2", menuName = "Data/RSE/Boss/OnFinishBossP2")]
+public class RSE_OnFinishBossP2 : BT.ScriptablesObject.RuntimeScriptableEvent{}
