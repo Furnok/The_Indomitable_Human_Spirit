30c7467a1ca83a3ada2f8c60ca1fba1711a1721e
MartinsFlorian <f.martins2005.fm@gmail.com>
2026-03-03 13:04:14 +0100

    Fix: Reflect

--- Assets/App/Scripts/Runtime/Boss/S_BossReflect.cs
+++ Assets/App/Scripts/Runtime/Boss/S_BossReflect.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 public class S_BossReflect : MonoBehaviour
 {
     [TabGroup("References")]
-    [SerializeField] private LayerMask reflectLayer;
-
-    [TabGroup("References")]
     [Title("Body")]
     [SerializeField] private GameObject body;
 
     private void OnTriggerEnter(Collider other)
     {
         // Search if the Object that Enter is Reflectible Projectile
-        if(other.TryGetComponent<I_ReflectableProjectile>(out var reflectable) && other.gameObject.layer == reflectLayer)
+        if(other.TryGetComponent<I_ReflectableProjectile>(out var reflectable))
         {
             if (reflectable.CanReflect() == false) return;
 
